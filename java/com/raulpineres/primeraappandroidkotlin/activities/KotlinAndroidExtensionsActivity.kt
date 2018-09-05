ddbebbf02f55056fdc84c5b4265bd286a76827f3
raul.pineres <raul0208@gmail.com>
2018-09-05 09:57:14 -0500

    Add toolbar in AndroidExtensions activity

--- java/com/raulpineres/primeraappandroidkotlin/activities/KotlinAndroidExtensionsActivity.kt
+++ java/com/raulpineres/primeraappandroidkotlin/activities/KotlinAndroidExtensionsActivity.kt
@@ -1,17 +1,20 @@
 package com.raulpineres.primeraappandroidkotlin.activities
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.Toolbar
 import android.widget.Button
 import android.widget.Toast
 import com.raulpineres.primeraappandroidkotlin.R
+import com.raulpineres.primeraappandroidkotlin.others.ToolbarActivity
 import kotlinx.android.synthetic.main.activity_kotlin_android_extensions.*
 
-class KotlinAndroidExtensionsActivity : AppCompatActivity() {
+class KotlinAndroidExtensionsActivity : ToolbarActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_kotlin_android_extensions)
+
+        toolbarToLoad(toolbar as Toolbar)
 
         val btn = findViewById<Button>(R.id.buttonById)
         btn.setOnClickListener{ Toast.makeText(this, "Click FindViewById", Toast.LENGTH_SHORT).show() }
